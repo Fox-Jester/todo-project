b9b673461299ff1689847a8f250864e339bd28ea
William Marshall <willmarshall2424@gmail.com>
2025-07-01 16:00:30 -0400

    updated project tab listeners

--- src/components/project/events/addProjectTabEvents.js
+++ src/components/project/events/addProjectTabEvents.js
@@ -12,11 +12,6 @@ export default function addProjectTabEvents(tab, createList){
 
 
    
-    const tabMenuGroup = tab.querySelector(".tab-menu-group");
-    const dropdown = tabMenuGroup.querySelector(".project-tab-dropdown");
 
-    tabMenuGroup.addEventListener("click", () => {
-        dropdown.classList.remove("hidden");
-    })
 
 }
