8fb6a288661454e961902532c289ad4025381ca3
William Marshall <willmarshall2424@gmail.com>
2025-07-05 10:44:27 -0400

    created getDate function

--- src/components/todo/elements/createTodoElement.js
+++ src/components/todo/elements/createTodoElement.js
@@ -23,9 +23,13 @@ export default function createTodoElement(title, description, date, time, priori
 
     const timeGroup = constructElement("div", "time-group");
 
-    const timeTag = document.createElement("p");
-    timeTag.textContent = time;
+    const dateTag = document.createElement("p");
+    dateTag.textContent = getDate()
 
+    const timeTag = document.createElement("p");
+    timeTag.textContent = getTime();
+
+    const timeIcon = constructElement("i", "fa-clock", "fa-regular")
 
 
 
@@ -34,18 +38,63 @@ export default function createTodoElement(title, description, date, time, priori
     const priorityTag = constructElement("div", "priority-tag", getPriorityColor(priority));
 
 
-    function getDateTag(){
-        const dateTag = document.createElement("p");
-        dateTag.textContent = date
-        return dateTag;
+    function getDate(){
+    if (!date) return "";
+    const [year, month, day] = date.split("-").map(Number);
+    const inputDate = new Date(year, month - 1, day);
+
+    const today = new Date();
+    const tomorrow = new Date();
+    tomorrow.setDate(today.getDate() + 1);
+
+  
+    const isSameDay = (d1, d2) =>
+        d1.getFullYear() === d2.getFullYear() &&
+        d1.getMonth() === d2.getMonth() &&
+        d1.getDate() === d2.getDate();
+
+    if (isSameDay(inputDate, today)) {
+        return "today";
+    }
+    if (isSameDay(inputDate, tomorrow)) {
+        return "tomorrow";
+    }
+    return date;
+    }
+
+    function getTime(){
+        if (time) {
+            const hour = parseInt(time.substring(0, 2), 10);
+            
+            if (hour === 0) {
+                return `12${time.substring(2)} am`;
+            }
+            if (hour < 12) {
+                return `${hour}${time.substring(2)} am`;
+            }
+            if (hour === 12) {
+                return `12${time.substring(2)} pm`;
+            }
+            return `${hour - 12}${time.substring(2)} pm`;
+        }
+        return "";
     }
 
     function getPriorityColor(priority){
         return (priority === "none" ? "" : `priority-${priority}`)
     }
+   
+    if (time || date) {
+        timeGroup.appendChild(timeIcon);
+    }
 
+    if (date) {
+        timeGroup.appendChild(dateTag);
+    }
 
-    timeGroup.append(getDateTag(), timeTag);
+    if (time) {
+        timeGroup.appendChild(timeTag);
+    }
 
 
     todoContent.append(todoTitle, todoDescription, timeGroup);
